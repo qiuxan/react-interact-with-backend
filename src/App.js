97f92c15c041ccbecc5fbd550a567091d3f074ca
qiuxan <qiuxan@gmail.com>
2021-10-01 08:25:00 +1000

    upload files

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  handleAdd = async () => {
+    const obj = { title: 't', body: 'b' };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+    console.log(post);
+    const posts = [post, ...this.state.posts]; //posts in state is an array
+    this.setState({ posts })
+  };
+
+  handleUpdate = post => {
+    console.log("Update", post);
+  };
+
+  handleDelete = post => {
+    console.log("Delete", post);
+  };
+
+  async componentDidMount() {
+    const response = await axios.get(apiEndpoint);
+    const { data } = response;
+    // console.log(data);
+    this.setState({ posts: data });
+
+  }
+
+  render() {
+    // console.log(this.state);
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
